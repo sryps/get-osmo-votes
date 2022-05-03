74254e08d1f46561bebc533f579cc55f796e9ff1
sryps <97310071+sryps@users.noreply.github.com>
2022-05-03 17:21:12 -0400

    Update get-osmo-votes.sh

--- get-osmo-votes.sh
+++ get-osmo-votes.sh
@@ -14,14 +14,14 @@ read -p "Enter the validator address (osmovaloper1vmkt...)  : " valAddress
 valAddress=${valAddress:-osmovaloper1vmkt6ysppk8m4rhlq78tpqyh429lhlsah4mn3y}
 
 printf "\n\nLooking for delegators..."
-osmosisd query staking delegations-to $valAddress --node $node 2> /dev/null | grep delegator_address | awk '{print $2}' > delegators.log
+osmosisd query staking delegations-to $valAddress --node $node | grep delegator_address | awk '{print $2}' > delegators.log
 
 num="$(wc -l < delegators.log)"
 printf "\n\nGetting votes from $num delegators...\n\n"
 
 while read p; do
   printf "\nCheck delegator $p..."
-  osmosisd query gov vote $prop $p --node $node 2> /dev/null | grep option | head -1 >> votes.log
+  osmosisd query gov vote $prop $p --node $node 2>/dev/null | grep option | head -1 >> votes.log
 done <delegators.log
 
 yes="$(grep -o -i YES votes.log | wc -l)"
