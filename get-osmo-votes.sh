d20ed1142f4d0ef2bbc4f62845bb1048130be898
sryps <97310071+sryps@users.noreply.github.com>
2022-05-03 18:34:39 -0400

    Update get-osmo-votes.sh

--- get-osmo-votes.sh
+++ get-osmo-votes.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 clear
-rm delegators.log 2> /dev/null
-rm votes.log 2> /dev/null
+rm delegators.log 2>/dev/null
+rm votes.log 2>/dev/null
 
 yes="0"
 no="0"
 abstain="0"
 node="https://osmosis.validator.network:443"
+offset=0
 
 printf "\n\n"
-read -p "Enter the active prop # to query (must be an active prop):  " prop
+read -p "Enter the active prop # to query:  " prop
 read -p "Enter the validator address (osmovaloper1vmkt...)  : " valAddress
 valAddress=${valAddress:-osmovaloper1vmkt6ysppk8m4rhlq78tpqyh429lhlsah4mn3y}
 
-printf "\n\nLooking for delegators..."
-osmosisd query staking delegations-to $valAddress --node $node | grep delegator_address | awk '{print $2}' > delegators.log
+osmosisd query staking delegations-to $valAddress --node $node --count-total | grep total | awk '{print $2}' | tr -d '"' > total.log
+total="$(cat total.log)"
+
+while [ $offset -le $total ]
+do
+  printf "\n\nLooking for delegators...$offset"
+  osmosisd query staking delegations-to $valAddress --node $node --offset $offset | grep delegator_address | awk '{print $2}' >> delegators.log
+  offset=$(( $offset + 100  ))
+  if [[ $offset == 500 ]]
+  then
+    break
+  fi
+done
 
 num="$(wc -l < delegators.log)"
 printf "\n\nGetting votes from $num delegators...\n\n"
@@ -39,5 +51,6 @@ printf "\n...$no delegators voted NO"
 
 printf "\n...$abstain delegators voted ABSTAIN\n\n"
 
-rm delegators.log 2> /dev/null
-rm votes.log 2> /dev/null
+rm delegators.log 2>/dev/null
+rm votes.log 2>/dev/null
+rm total.log 2>/dev/null
